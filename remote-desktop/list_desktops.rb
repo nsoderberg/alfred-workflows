c0bab8d03e271ee6bf8289f96e0ebca21f4b96aa
Niklas Söderberg <niklas@fnudd.com>
2018-11-17 14:08:30 +0100

    Rewrite for new Microsoft Remote Desktop client.

--- remote-desktop/list_desktops.rb
+++ remote-desktop/list_desktops.rb
@@ -1,39 +1,27 @@
 require './alfred_feedback.rb'
+require 'sqlite3'
 query = ''
 if ARGV.length > 0
-	query = ARGV[0].downcase
+    query = ARGV[0].downcase
 end
 home = `echo $HOME`.chomp
-raw_bookmarks = `plutil -p "#{home}/Library/Containers/com.microsoft.rdc.mac/Data/Library/Preferences/com.microsoft.rdc.mac.plist" | grep "bookmark"`.lines
-bookmarks = {}
-raw_bookmarks.each do |line| 
-	tokens = line.split("\"")
-	bookmark_id = tokens[1].split(/[{}]/)[1]
-	bookmark_key = tokens[1].split(/\./)[3]
-	bookmark_value = tokens[3]
-	bookmarks[bookmark_id] = (bookmarks[bookmark_id] || {})
-	if bookmark_key != nil
-		bookmarks[bookmark_id][bookmark_key.to_sym] = bookmark_value
-	end
-
-	title = bookmarks[bookmark_id][:title]
-	if bookmark_key == 'label' && bookmark_value.length > 0
-		bookmarks[bookmark_id][:title] = bookmark_value
-	end
-	if bookmark_key == 'hostname' && title == nil
-		bookmarks[bookmark_id][:title] = bookmark_value
-	end
-end
-desktops = bookmarks.reject {|entry| entry.nil? }.collect { |key, value| value[:title] }
-if query.length > 0
-	desktops = desktops.find_all {|desktop| desktop.downcase.include?(query)}
-end
 feedback = Feedback.new
-if desktops.size() > 0
-	desktops.each do |desktop|
-		feedback.add_item({:title => desktop, :subtitle => "Open desktop"})
-	end
-else
-	feedback.add_item({:title => 'No matching desktop found', :subtitle => "Can't open desktop", :arg => '##notfound##'})
+begin
+    db = SQLite3::Database.open "/Users/niklas/Library/Containers/com.microsoft.rdc.macos/Data/Library/Application\ Support/com.microsoft.rdc.macos/com.microsoft.rdc.application-data.sqlite"
+    stm = db.prepare "SELECT ZHOSTNAME FROM ZBOOKMARKENTITY WHERE ZHOSTNAME LIKE '%#{query}%'"
+rs = stm.execute 
+    
+    found = $false
+rs.each do |row|
+        feedback.add_item({:title => row[0], :subtitle => "Open desktop"})
+        found = $true
+    end
+    if found
+        feedback.add_item({:title => 'No matching desktop found', :subtitle => "Can't open desktop", :arg => '##notfound##'})
+    end
+    
+    ensure
+        stm.close if stm
+        db.close if db
 end
 puts feedback.to_xml
